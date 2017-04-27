aa2f734d8b5c694fa4036aa6a216c00e1d54b860
hongxiao95 <hongxiao95@hotmail.com>
2017-04-27 18:49:36 +0900

    添加项目文件。

---
+++ VideoCompresserSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VideoCompresserSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
